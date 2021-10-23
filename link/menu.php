5a6e295becf4795f7f2e5322ff8e08a15864377b
Fidelis <84164141+FidelisSantos@users.noreply.github.com>
2021-10-23 19:47:03 -0300

    Add files via upload

---
+++ link/menu.php
@@ -0,0 +1,13 @@
+<ul class="menu">
+  <?php if ($page != "home") : ?>
+  <li><a href="index.php">Home</a></li>
+  <?php endif; ?>
+  <?php if ($page != "conhecimento") : ?>
+  <li>
+    <a href="menu.php">Conhecimentos</a>
+  </li>
+  <?php endif; ?>
+  <li>
+    <a href="avaliacao.php">Avaliar Site</a>
+  </li>
+</ul>
---
+++ link/social.php
@@ -0,0 +1,22 @@
+</main>
+<div class="social">
+
+  <h3>Redes sociais</h3>
+
+  <ul>
+    <li>
+      <a href="https://www.facebook.com/vitorfidelis" target="_blank">
+        <img src="/Image/social/facebook.png" alt="facebook" />
+      </a>
+    </li>
+    <li>
+      <a href="https://github.com/FidelisSantos" target="_blank">
+        <img src="/Image/social/github.png" alt="GitHub" />
+      </a>
+    </li>
+    <li>
+      <a href="https://www.linkedin.com/in/vitor-fidelis-71b65b208/" target="_blank">
+        <img src="/Image/social/linkedin.png" alt="facebook" />
+    </li>
+  </ul>
+</div>
